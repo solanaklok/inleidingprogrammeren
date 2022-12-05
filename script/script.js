bdfa53d22257601e0097022db81016da9abaad2a
solanaklok <solana.klok@hva.nl>
2022-12-05 10:27:46 +0100

    push 4
    4 de commit

--- script/script.js
+++ script/script.js
@@ -1,1 +1,19 @@
-console.log('hallo')
+console.log('script')
+
+var toonStempel = []
+
+var pElement = document.querySelector('.naam');
+
+var newyork = document.querySelector('stempel new york');
+
+var RepubliqueFrancaise = document.querySelector('stempel Republique');
+
+var Spain = document.querySelector('.button');
+
+console.log(Spain)
+
+function toonStempel(){
+    
+}
+
+newyork.addEventListener("click",toonStempel,false);
